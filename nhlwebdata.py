7733c8d460cfa0b9d4f6e510aaf6bf07d48a9e62
zmedvinsky <zmed@umich.edu>
2022-04-21 13:08:09 -0400

    first commit

--- nhlwebdata.py
+++ nhlwebdata.py
@@ -1,1 +1,16 @@
+import requests
+import json
+import sqlite3
+import os
 
+def set_up_db(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
+
+def main():
+    cur, conn = set_up_db("FPData.db")
+
+if __name__ == "__main__":
+    main()
